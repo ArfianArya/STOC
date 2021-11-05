29851b5245576edf270a8041577fde15e60eb08b
Arfian Arya <arfianarya@gmail.com>
2021-11-06 00:20:06 +0800

    Adding Contact and Footer

--- apps.js
+++ apps.js
@@ -1,10 +1,10 @@
 // Select The Elements
-var toggle_btn;
+// var toggle_btn;
 var big_wrapper;
 var hamburger_menu;
 
 function declare() {
-  toggle_btn = document.querySelector(".toggle-btn");
+  // toggle_btn = document.querySelector(".toggle-btn");
   big_wrapper = document.querySelector(".big-wrapper");
   hamburger_menu = document.querySelector(".hamburger-menu");
 }
